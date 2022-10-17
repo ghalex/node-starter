5b1ad175c6bf624881db1125233f68479e0ecaff
Alexandru Ghiura <ghalex@gmail.com>
2022-10-17 12:24:36 +0300

    feat: add commitlint

---
+++ commitlint.config.cjs
@@ -0,0 +1,20 @@
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'] // => commitlint-config-example,
+  // rules: {
+  //   'type-enum': [
+  //     2,
+  //     'always',
+  //     [
+  //       'fix',
+  //       'feat',
+  //       'build',
+  //       'docs',
+  //       'refactor',
+  //       'revert',
+  //       'style',
+  //       'test'
+  //     ]
+  //   ]
+  // }
+}
